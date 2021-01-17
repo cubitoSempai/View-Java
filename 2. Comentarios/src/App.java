b64c25a38490de8d982219881c80d8c5feea9a89
Christian Mesa Garay <iworkinginaprogram@gmail.com>
2021-01-17 17:57:42 -0500

    Primer commit
    Se trabaja en los proyectos de:
    
    1. Hola mundo
    2. Comentarios
    3. Variables
    
    Notas. Se agrega el README.md

---
+++ 2. Comentarios/src/App.java
@@ -0,0 +1,22 @@
+public class App {
+    /*
+     * Una caracteristica de los comentarios es que al ejecutar nuestros programas
+     * no se tienen en cuenta, por lo tanto no tendran influencia alguna en el
+     * funcionamiento del programa
+     */
+
+    /*
+     * ¿porque usamos comentarios? La utilidad de los comentarios en Java como en
+     * todos los lenguajes es ayudar a entender o explicar de que forma funciona el
+     * codigo para otros programadores como a nosotros mismos
+     */
+
+    // Un ejemplo de comentarios en JAVA para nuestro Hola Mundo seria el siguiente
+
+    // Metodo principal, toda ejecucion primero pasa por este metodo
+    public static void main(String[] args) throws Exception {
+        // Este es un Hola mundo
+        System.out.println("Hello, World!");
+    }
+
+}
---
+++ 3. Variables/src/App.java
@@ -0,0 +1,28 @@
+public class App {
+    // scope global
+    public static void main(String[] args) throws Exception {
+        // scope local
+
+        // Informacion tipos de datos primitivos
+        // https://www.abrirllave.com/java/tipos-de-datos-primitivos.php
+
+        // Convenciones para nombrar variables
+        // * Empiezan con letra, $ o _
+        // * Son key Sensitive
+        // * LowerCamelCase
+
+        // Tipos de datos primitivos
+        // byte nombreVariable;
+        // short nombreVariable;
+        // int nombreVariable;
+        // long nombreVariable;
+        // float nombreVariable;
+        // double nombreVariable;
+        // boolean nombreVariable;
+        // char nombreVariable;
+
+        // No primitivo (Objetos)
+        // String
+        // string nombreVariable;
+    }
+}
